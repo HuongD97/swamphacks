6da6761e43ecbf99bb6a74155dd3c76dcad17a97
Christopher Feltner <chris.feltner@Knights.ucf.edu>
2019-01-19 08:55:44 -0500

    added routes directory

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+var indexRouter = require("./routes/index");
+
 app.prepare()
     .then(() => {
         const server = express();
