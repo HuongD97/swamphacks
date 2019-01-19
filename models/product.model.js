57926a1392f332f26f4b34006a4e34da30a2f0b1
Christopher Feltner <chris.feltner@Knights.ucf.edu>
2019-01-19 15:05:20 -0500

    product model

---
+++ models/product.model.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+let ProductSchema = new schema({
+    name: {type: String, required: true, max: 100},
+    provider_id: {type: String, required: true},
+    quantity: {type: Number, required: true},
+    image_url: {type: String, required: false},
+    category_id: {type: String, required: true}
+})
+
+let Product = module.exports = mongoose.model('Product', ProductSchema);
+
+let getAvailableProducts = function(arr) {
+    var availableProducts = [];
+    for (var product in arr)
+    {
+        if(arr[product].quantity > 0)
+        {
+            availableProducts.push(arr[product]);
+        }
+    }
+    return availableProducts;
+}
+
+module.exports.addProduct = function(product, callback) {
+    Product.create(product);
+}
+
+module.exports.getAllAvailable = async function(limit) {
+    const arr = await Product.find().limit(limit);
+    return getAvailableProducts(arr);
+}
+
+module.exports.getByCategory = async function(category, limit) {
+    const arr = await Product.find({ category_id: category})
+    return getAvailableProducts(arr);
+}
+
+module.exports.getById = function(id, callback) {
+    Product.findById(id, callback);
+}
+
+module.exports.updateProduct = async function(id, product) {
+    return await Product.findOneAndUpdate(id, product);
+}
--- server.js
+++ server.js
@@ -5,33 +5,22 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-var indexRouter = require("./routes/index");
+//configure MongoDB
+let mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_DB_URL);
+let db = mongoose.connection;
 
 app.prepare()
     .then(() => {
         const server = express();
         server.use(bodyParser.json());
         
-        const item1 = {
-            id: 1,
-            name: 'Banana',
-            quantity: 5
-        };
-        const item2 = {
-            id: 2,
-            name: 'Tomato',
-            quantity: 5
-        };
+        // set up routes
+        var indexRouter = require("./routes/index");
+        var productRouter = require("./routes/product");
 
-        const allItems = [item1, item2];
-
-        server.post('/getItem', (req, res) => {
-            const { id } = req.body;
-            const result = allItems.find((item) => {
-                return item.id === id;
-            });
-            res.json(result);
-        });
+        server.use('/', indexRouter);
+        server.use('/product', productRouter);
 
         server.get('*', (req, res) => {
             return handle(req, res);
